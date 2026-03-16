3f41fac2aff9ba9c203e35e1d14b88325a5c5c6b
andresjuntos@gmail.com <andresjuntos@gmail.com>
2026-03-16 18:11:12 -0500

    refactor: mover HTMLs a raiz de frontend - URLs limpias sin /pages/

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/*.html", //Buscar en todos los HTML
-    "./src/scripts/*.js"  //Buscar en todos los JS
+    "./*.html",       //Buscar en todos los HTML
+    "./scripts/*.js"  //Buscar en todos los JS
   ],
   theme: {
     extend: {}, //Personalizar colores, fuentes, ect.
